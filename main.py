b9509ee12b3c46b9ae213df5ee84a40497a844bd
Harshitha Hariharan <harshithahariharan75@gmail.com>
2026-05-23 21:48:13 +0530

    Added main file

---
+++ main.py
@@ -0,0 +1,32 @@
+# This file starts the application,
+# initializes the database connection,
+# and launches the main application window.
+
+
+# Import system module for handling application execution and exit.
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMessageBox
+from database import init_db
+from app import ExpenseApp
+
+# creating the main function to start the application.
+
+def main():   
+    app = QApplication(sys.argv) 
+    
+    if not init_db("expense.db"):
+        QMessageBox.critical(None, "Error", "Could not load your database...")
+        sys.exit(1)
+                        
+        
+
+    window = ExpenseApp()  
+    window.show()          
+
+    sys.exit(app.exec())   
+
+if __name__ == "__main__":
+    main()
+
+
